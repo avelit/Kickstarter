3d7ba04e6f26ca7b9f3760bd17f30ca9881b9bae
Velytchenko Oleksandr <avelit@ukr.net>
2015-04-11 19:21:28 +0300

    dependency

--- src/main/java/ua/goit/gojava32/kickstarter/servlet/servlet.java
+++ src/main/java/ua/goit/gojava32/kickstarter/servlet/servlet.java
@@ -2,10 +2,18 @@ package ua.goit.gojava32.kickstarter.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import ua.goit.gojava32.kickstarter.view.View;
 
 public class servlet extends HttpServlet {
+
+  private static final long serialVersionUID = -5152327662872804857L;
 
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
