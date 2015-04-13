940e254ac4ec0eebd530306cacd78d45bd6b724a
Ivan Kucherenko <warrax_net@ukr.net>
2015-04-13 18:03:34 +0300

    Add new servlet

---
+++ src/main/java/ua/goit/gojava32/kickstarter/servlet/KickstarterServlet.java
@@ -0,0 +1,27 @@
+package ua.goit.gojava32.kickstarter.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ua.goit.gojava32.kickstarter.view.HtmlGenerator;
+
+
+@WebServlet("/categories/*")
+public class KickstarterServlet extends HttpServlet {
+
+  private static final long serialVersionUID = -5152327662872804857L;
+
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    PrintWriter pw = response.getWriter();
+    pw.println("<html><body>");
+    pw.println(HtmlGenerator.getHtml(request.getRequestURI()));
+    pw.println("</body></html>");
+  }
+}
