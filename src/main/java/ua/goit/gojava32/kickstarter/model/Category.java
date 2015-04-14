60881800c6cb318385b062a3653f4a8de56d3b41
Velytchenko Oleksandr <avelit@ukr.net>
2015-04-14 10:36:58 +0300

    model

--- src/main/java/ua/goit/gojava32/kickstarter/model/Category.java
+++ src/main/java/ua/goit/gojava32/kickstarter/model/Category.java
@@ -1,5 +1,8 @@
 package ua.goit.gojava32.kickstarter.model;
 
-public class Category {
+import java.util.List;
 
+public class Category {
+  String name;
+  List<Project> projects;
 }
--- src/main/java/ua/goit/gojava32/kickstarter/model/Project.java
+++ src/main/java/ua/goit/gojava32/kickstarter/model/Project.java
@@ -1,5 +1,6 @@
 package ua.goit.gojava32.kickstarter.model;
 
 public class Project {
-
+  String name;
+  Category category;
 }
