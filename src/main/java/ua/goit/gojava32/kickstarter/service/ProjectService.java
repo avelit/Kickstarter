b071857d0553ae48a3488366d0ee4a59fd7f09cd
Velytchenko Oleksandr <avelit@ukr.net>
2015-04-15 10:10:15 +0300

    project service

--- src/main/java/ua/goit/gojava32/kickstarter/service/ProjectService.java
+++ src/main/java/ua/goit/gojava32/kickstarter/service/ProjectService.java
@@ -6,9 +6,9 @@ import ua.goit.gojava32.kickstarter.model.Category;
 import ua.goit.gojava32.kickstarter.model.Project;
 
 public interface ProjectService {
-  Project addProject(Project project);
+  Project add(Project project);
   List<Project> findAll(Category category);
   List<Project> findAll(Integer id);//id category
-  Project deleteProject(Project project);
-  Project getProject(Integer id);
+  Project delete(Project project);
+  Project get(Integer id);
 }
