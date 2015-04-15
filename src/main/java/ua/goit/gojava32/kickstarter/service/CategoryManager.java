0b01644c65f98a8ba8295ccf40e3cf6d84aeb98a
Ivan Kucherenko <warrax_net@ukr.net>
2015-04-16 01:13:12 +0300

    Add impl for Categories and Projects

---
+++ src/main/java/ua/goit/gojava32/kickstarter/service/CategoryManager.java
@@ -0,0 +1,59 @@
+package ua.goit.gojava32.kickstarter.service;
+
+import ua.goit.gojava32.kickstarter.model.Category;
+import ua.goit.gojava32.kickstarter.model.Project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CategoryManager implements CategoryService{
+
+    List<Category> categories = new ArrayList<Category>();
+
+    @Override
+    public Category add(Category category) {
+        categories.add(category);
+        return category;
+    }
+
+    @Override
+    public Category update(Category category) {
+        for (int i = 0; i < categories.size(); i++){
+            if (categories.get(i).getName().equals(category.getName())){
+                categories.set(i, category);
+            }
+        }
+        return category;
+    }
+
+    @Override
+    public List<Category> findAll() {
+        return null;
+    }
+
+    @Override
+    public List<Project> findAllProjects(Category category) {
+        return category.getProjects();
+    }
+
+    @Override
+    public List<Project> findAllProjects(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Category delete(Category category) {
+        categories.remove(category);
+        return category;
+    }
+
+    @Override
+    public Category get(Integer id) {
+        for (Category cat : categories) {
+            if (cat.getId() == id){
+                return cat;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/ua/goit/gojava32/kickstarter/service/ProjectManager.java
@@ -0,0 +1,40 @@
+package ua.goit.gojava32.kickstarter.service;
+
+import ua.goit.gojava32.kickstarter.model.Category;
+import ua.goit.gojava32.kickstarter.model.Project;
+
+import java.util.List;
+
+public class ProjectManager implements ProjectService {
+
+
+    @Override
+    public Project add(Project project) {
+        return null;
+    }
+
+    @Override
+    public Project update(Project project) {
+        return null;
+    }
+
+    @Override
+    public List<Project> findAll(Category category) {
+        return null;
+    }
+
+    @Override
+    public List<Project> findAll(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Project delete(Project project) {
+        return null;
+    }
+
+    @Override
+    public Project get(Integer id) {
+        return null;
+    }
+}
