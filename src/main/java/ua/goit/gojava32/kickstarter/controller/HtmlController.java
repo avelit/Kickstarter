584fdd9fd6f41ea10bd63afd68bacab0bf6d7972
zozich <zozich2010@gmail.com>
2015-04-17 00:35:10 +0300

    controller v 1.6

--- src/main/java/ua/goit/gojava32/kickstarter/controller/HtmlController.java
+++ src/main/java/ua/goit/gojava32/kickstarter/controller/HtmlController.java
@@ -43,12 +43,12 @@ public class HtmlController {
   private String processCategories() {
     String jspName = "";
     String categoryName = uriSegments[2];
+    request.setAttribute("category_name", categoryName);
 
     if (uriSegments.length == 3) {
       jspName = "category.jsp";
       request.setAttribute("projects", (new DataProvider()).getListByCategory(categoryName));
     } else {
-      request.setAttribute("category_name", categoryName);
       jspName = processProjects();
     }
     return jspName;
