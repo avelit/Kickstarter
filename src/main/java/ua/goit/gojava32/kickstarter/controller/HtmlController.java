1b6996e0aac2a3e8fdcc35e24a0647dea21b6081
zozich <zozich2010@gmail.com>
2015-04-16 17:22:48 +0300

    controller v 1.0

--- src/main/java/ua/goit/gojava32/kickstarter/controller/HtmlController.java
+++ src/main/java/ua/goit/gojava32/kickstarter/controller/HtmlController.java
@@ -1,15 +1,9 @@
 package ua.goit.gojava32.kickstarter.controller;
 
 import java.io.IOException;
-import java.util.List;
-import java.util.Set;
-
 import ua.goit.gojava32.kickstarter.service.DataProvider;
-
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 public class HtmlController {
   /*
