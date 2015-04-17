4aa6a57316036d65624dac718ed30e169aa73fb8
Velytchenko Oleksandr <avelit@ukr.net>
2015-04-17 14:01:08 +0300

    ident

--- src/main/java/ua/goit/gojava32/kickstarter/model/Category.java
+++ src/main/java/ua/goit/gojava32/kickstarter/model/Category.java
@@ -3,28 +3,28 @@ package ua.goit.gojava32.kickstarter.model;
 import java.util.List;
 
 public class Category {
-    private String name;
-    private List<Project> projects;
-    private Integer id;
+  private String name;
+  private List<Project> projects;
+  private Integer id;
 
-    public Category(Integer id, String name){
-        this.id = id;
-        this.name = name;
-    }
+  public Category(Integer id, String name){
+    this.id = id;
+    this.name = name;
+  }
 
-    public Integer getId() {
-        return id;
-    }
+  public Integer getId() {
+    return id;
+  }
 
-    public void setName(String name) {
-        this.name = name;
-    }
+  public void setName(String name) {
+    this.name = name;
+  }
 
-    public String getName() {
-        return name;
-    }
+  public String getName() {
+    return name;
+  }
 
-    public List<Project> getProjects () {
-        return projects;
-    }
+  public List<Project> getProjects () {
+    return projects;
+  }
 }
