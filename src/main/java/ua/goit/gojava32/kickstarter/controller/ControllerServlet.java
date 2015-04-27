4f003141a653ee5d1a7aee3bf97557c9eb0c1d0b
Velytchenko Oleksandr <avelit@ukr.net>
2015-04-27 16:25:50 +0300

    fix

--- src/main/java/ua/goit/gojava32/kickstarter/controller/ControllerServlet.java
+++ src/main/java/ua/goit/gojava32/kickstarter/controller/ControllerServlet.java
@@ -26,7 +26,7 @@ public class ControllerServlet {
 
   public void handleRequest() throws ServletException, IOException {
 
-    Controller controller = ControllerMap.getController(request.getMethod().toLowerCase() + uriSegments[uriSegments.length - 1]);
+    Controller controller = ControllerMap.getController(request.getMethod().toLowerCase() + "_" + uriSegments[uriSegments.length - 1]);
     ViewModel vm = controller.process(request);
 
     if (vm.getCommand().equals("sendRedirect")) {
