88778b323f7ebc9e68ad2e21944201c0fc31666b
Denis Gaidak <cockroa4es@gmail.com>
2015-04-27 17:36:55 +0300

    edit kickstarteServelet

--- src/main/java/ua/goit/gojava32/kickstarter/servlet/KickstarterServlet.java
+++ src/main/java/ua/goit/gojava32/kickstarter/servlet/KickstarterServlet.java
@@ -2,14 +2,16 @@ package ua.goit.gojava32.kickstarter.servlet;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import ua.goit.gojava32.kickstarter.controller.Controller;
 import ua.goit.gojava32.kickstarter.controller.ControllerMap;
-import ua.goit.gojava32.kickstarter.controller.ControllerServlet;
+import ua.goit.gojava32.kickstarter.model.User;
 import ua.goit.gojava32.kickstarter.view.ViewModel;
 
 //@WebServlet("/categories/*")
@@ -17,24 +19,30 @@ public class KickstarterServlet extends HttpServlet {
   private static final long serialVersionUID = -5152327662872804857L;
 
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//    (new ControllerServlet(request, response)).handleRequest();
-
-    handle(request, response);
+    handleRequest(request, response);
   }
 
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//    (new ControllerServlet(request, response)).handleRequest();
-
-    handle(request, response);
+    handleRequest(request, response);
   }
 
-  private void handle(HttpServletRequest request, HttpServletResponse response) {
+  private void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-    String mapKey = request.getMethod() + "_" + request.getRequestURI();
-    Controller controller = ControllerMap.getController(mapKey);
-    ViewModel viewModel = controller.process(request);
+    String[] uriSegments = request.getRequestURI().split("/");
+    Controller controller = ControllerMap.getController(request.getMethod().toLowerCase() + "_" + uriSegments[uriSegments.length - 1]);
+    ViewModel vm = controller.process(request);
 
+    User user = (User)request.getAttribute("user");
+    if (user != null){
+      response.addCookie(new Cookie("token", user.getToken()));
+    }
 
+    if (vm.getCommand().equals("sendRedirect")) {
+      response.sendRedirect(vm.getView());
+    } else if (vm.getCommand().equals("forward")) {
+      RequestDispatcher dispatcher = request.getRequestDispatcher(vm.getView());
+      dispatcher.forward(request, response);
+    }
   }
 
 }
