ba9313d8847c22560cc6b85f26bb013b7842953a
Velytchenko Oleksandr <avelit@ukr.net>
2015-05-14 10:48:49 +0300

    redundant

--- src/main/java/ua/goit/gojava32/kickstarter/controller/SearchController.java
+++ src/main/java/ua/goit/gojava32/kickstarter/controller/SearchController.java
@@ -1,20 +1,22 @@
 package ua.goit.gojava32.kickstarter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-import ua.goit.gojava32.kickstarter.factory.ServiceModel;
 import ua.goit.gojava32.kickstarter.service.CategoryService;
 import ua.goit.gojava32.kickstarter.service.ProjectService;
 
 @Controller
 public class SearchController {
 
-  CategoryService categoryService = ServiceModel.getCategoryService();
-  ProjectService projectService = ServiceModel.getProjectService();
+  @Autowired
+  private CategoryService categoryService;
+  @Autowired
+  private ProjectService projectService;
 
   @RequestMapping(value = "/search", method = RequestMethod.GET)
   @ResponseBody
