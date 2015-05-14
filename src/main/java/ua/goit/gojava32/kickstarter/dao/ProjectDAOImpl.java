cca687b4ccbf75754b0f7e829deee5cbdc72436b
zozich <zozich2010@gmail.com>
2015-05-14 14:47:14 +0300

    ProjectDAOImpl updated

--- src/main/java/ua/goit/gojava32/kickstarter/dao/ProjectDAOImpl.java
+++ src/main/java/ua/goit/gojava32/kickstarter/dao/ProjectDAOImpl.java
@@ -1,24 +1,11 @@
 package ua.goit.gojava32.kickstarter.dao;
 
-import org.apache.log4j.Logger;
 import org.hibernate.Query;
 import org.hibernate.Session;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-import ua.goit.gojava32.kickstarter.connections.ConnectionPool;
-import ua.goit.gojava32.kickstarter.factory.FactoryModel;
-import ua.goit.gojava32.kickstarter.model.Category;
 import ua.goit.gojava32.kickstarter.model.Project;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 @Transactional
 @Repository
