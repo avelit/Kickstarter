7d375245e914fd2c783fefb28a5d39399e2583bf
Ivan Kucherenko <warrax_net@ukr.net>
2015-05-18 01:39:46 +0300

    Add test video

--- src/main/resources/sql/insertData.sql
+++ src/main/resources/sql/insertData.sql
@@ -4,7 +4,7 @@ INSERT INTO categories (id, name, description) VALUES (45, 'Tech', 'test descrip
 INSERT INTO categories (id, name, description) VALUES (46, 'Food', 'food desc');
 
 
-INSERT INTO projects (id, name, id_category, id_author, description) VALUES (1, 'project 1', 1, 1, 'bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-');
+INSERT INTO projects (id, name, id_category, id_author, description, video) VALUES (1, 'project 1', 1, 1, 'bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-bla-', 'https://www.youtube.com/embed/J4L4UQnbjGc');
 INSERT INTO projects (id, name, id_category, id_author, description) VALUES (22, 'testproject 2', 1, 1, 'test description');
 
 INSERT INTO blogs (id, author, id_project, text, created) VALUES (1, '1', 1, 'blablabla_blog', '1430946000000');
