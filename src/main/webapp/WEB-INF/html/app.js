85a82218be98557d18eeb4824fdad660f7e1a495
Velytchenko Oleksandr <avelit@ukr.net>
2015-06-02 12:50:07 +0300

    angular

---
+++ src/main/webapp/WEB-INF/html/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+(function () {
+
+    var app = angular.module('kickstarter', ['ngRoute']);
+
+    app.config(['$routeProvider', '$locationProvider',
+
+        function ($routeProvider, $locationProvider) {
+            $routeProvider
+                .when('/html', {templateUrl: '/html/index.html'})
+                .when('/html/category', {templateUrl: '/html/categories.html'})
+                .otherwise({redirectTo: '/'});
+
+            $locationProvider.html5Mode({enabled: true, requireBase: false});
+
+        }]);
+
+    app.controller('CategoryListCtrl', ['$http', function ($http) {
+        var ctrl = this;
+        ctrl.list = [];
+        $http.get('/webapi/category').success(function (data) {
+            ctrl.list = data.categories;
+        });
+    }]);
+
+    app.directive('navMenu',function(){
+        return{
+            restrict: 'E',
+            templateUrl:'/html/header.html'
+        }
+    })
+
+})();
